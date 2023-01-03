6506134b76e6e8172990454cf38537b31c488e8c
boguss1225 <jcjc7890@gmail.com>
2023-01-03 12:50:37 +1100

    update train

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # some training parameters
 EPOCHS = 1000
-BATCH_SIZE = 1
+BATCH_SIZE = 16
 NUM_CLASSES = 3
 image_height = 128
 image_width = 128
 channels = 3
 
 model_save_name = "TestNet"
-model_dir = "trained_models/IMAS_Salmon/"+model_save_name+"/" # = save_dir
+model_dir = "trained_models/ivy_coverage/"+model_save_name+"/" # = save_dir
 
 dir_base = "/home/mirap/0_DATABASE/ivy_coverage/cropped/"
 train_dir = dir_base + "set1"
 valid_dir = dir_base + "5_folds/1fold"
 test_dir = dir_base + "test"
-test_image_path = dir_base + "test/1/untitled-1054_2053_679_1.jpg"
+test_image_path = dir_base + "test/ivy/resultsivy_006_2112.jpg"
